40e24fb7a0faf426c6dec07ffbb63e4f74ffe854
gdi2290 <github@gdi2290.com>
2015-03-14 13:56:08 -0700

    fix: check if defined rather than falsey

--- angular-async.js
+++ angular-async.js
@@ -4,7 +4,9 @@
     function $asyncFilter(async) {
       if (angular.isDefined(async) && async.then) {
         async.$$state = async.$$state || {};
-        async.$$state.value = async.$$state.value || undefined;
+        async.$$state.value = angular.isDefined(async.$$state.value) ?
+                                                 async.$$state.value :
+                                                           undefined ;
         return async.$$state.value;
       }
       return async;
