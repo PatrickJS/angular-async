1a688aa87ee5ee26e8671ebd0c1d1e77e5671f9d
gdi2290 <github@gdi2290.com>
2015-03-14 13:46:49 -0700

    chore: create source mvp

---
+++ angular-async.js
@@ -0,0 +1,22 @@
+(function() {
+
+  function $Async() {
+    function $asyncFilter(async) {
+      if (angular.isDefined(async) && async.then) {
+        async.$$state = async.$$state || {};
+        async.$$state.value = async.$$state.value || undefined;
+        return async.$$state.value;
+      }
+      return async;
+    }
+    return $asyncFilter;
+  }
+
+
+  angular.module('ngAsync', [])
+  .filter('async', [$Async])
+  .filter('await', [$Async]);
+
+  angular.module('angular-async', ['ngAsync']);
+
+}());
